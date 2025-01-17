199a1670a74ff72dd8e640f2ebd890ac798542ea
Romakumari17 <138012027+Romakumari17@users.noreply.github.com>
2025-01-17 23:52:24 +0530

    commit

--- app/src/main/java/com/romakumari/signinsignuppageapp/SignInFragment.kt
+++ app/src/main/java/com/romakumari/signinsignuppageapp/SignInFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.romakumari.signinsignuppageapp.databinding.FragmentSignInBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +18,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SignInFragment : Fragment() {
+    lateinit var binding: FragmentSignInBinding
+    var ischeck=false
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +37,7 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        return binding.root
     }
 
     companion object {
@@ -54,6 +57,17 @@ class SignInFragment : Fragment() {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
+                binding.btneye.setOnClickListener {
+                    isConfirmVisible = !isConfirmVisible
+                    if (isConfirmVisible){
+                        binding.inputPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                        binding.iveyeclosedconf.setImageResource(R.drawable.eye)
+                    }else{
+                        binding.inputPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                        binding.iveyeclosedconf.setImageResource(R.drawable.img)
+                    }
+                    binding.etconfpassword.setSelection(binding.etconfpassword.text.length)
+                            }
             }
     }
 }
